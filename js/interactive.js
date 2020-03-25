6ab568a7142dc34da9a2b314c7a940a903804f46
porrocas <u1802833@unimilitar.edu.co>
2020-03-25 14:29:40 -0500

    [add] add news functions

--- js/interactive.js
+++ js/interactive.js
@@ -1,24 +1,49 @@
+//variables globales
+const animationScrolltop = document.getElementsByClassName('animationsScrollTop');
+const appFace = document.getElementById('appFace');
+
 // eventos
-document.addEventListener('DOMContentLoaded', function() {
-    // menu de movil
-    const elems = document.querySelectorAll('.sidenav');
-    const instances = M.Sidenav.init(elems);
+addEvents();
 
-    // parallax
-    reloadImgQuerys();
-    var elems1 = document.querySelectorAll('.parallax');
-    var instances1 = M.Parallax.init(elems1);
+function addEvents() {
+    document.addEventListener('DOMContentLoaded', function() {
+        // menu de movil
+        const elems = document.querySelectorAll('.sidenav');
+        const instances = M.Sidenav.init(elems);
 
-    // formulario cotizaciones
-    var elems2 = document.querySelectorAll('select');
-    var instances2 = M.FormSelect.init(elems2);
-});
+        //slider
+        var elemsSlider = document.querySelectorAll('.carousel');
+        var instancesSlider = M.Carousel.init(elemsSlider);
+
+        // formulario cotizaciones
+        var elems2 = document.querySelectorAll('select');
+        var instances2 = M.FormSelect.init(elems2);
+
+        removeClassResponsive();
+    });
+    window.addEventListener('scroll', () => {
+        console.log()
+        if (window.scrollY > animationScrolltop[0].scrollHeight + 20) {
+            animationScrolltop[0].classList.add('slideRight');
+        }
+        if (window.scrollY > animationScrolltop[1].scrollHeight + 320) {
+            animationScrolltop[1].classList.add('slideRight');
+        }
+        if (window.scrollY > animationScrolltop[2].scrollHeight + 620) {
+            animationScrolltop[2].classList.add('slideRight');
+        }
+    })
+    appFace.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (e.target.classList.contains('startApp')) {
+            // click en boton start app
+        }
+    })
+}
 
 //funciones
-//funcion para cambiar imagen segun tamaño de pantalla
-function reloadImgQuerys() {
-    if (screen.width <= 425) {
-        document.getElementById('imgParallax1').src = './img/ps0.jpg';
-        document.getElementById('imgParallax2').src = './img/ps1.jpg';
+function removeClassResponsive() {
+    if (screen.width < 500) {
+        document.getElementById('containerMap').classList.remove('valign-wrapper');
     }
 }
